bc9b4dd8449661c67f09a7669dd8a1ec2d7f0356
Venkatesh Prasath <54813384+venkatesh2110@users.noreply.github.com>
2020-08-14 20:00:34 +0530

    restuarant card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,15 @@ import {MatGridListModule} from '@angular/material/grid-list';
 import {MatButtonModule} from '@angular/material/button';
 
 import {MatSidenavModule} from '@angular/material/sidenav';
-
+import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatInputModule} from '@angular/material/input';
 import {MatDialogModule} from '@angular/material/dialog';
 import { LogincomponentComponent } from './logincomponent/logincomponent.component';
 import { SignupcomponentComponent } from './signupcomponent/signupcomponent.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatSelectModule} from '@angular/material/select';
+import {MatCardModule} from '@angular/material/card';
 
 
 @NgModule({
@@ -39,7 +41,10 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
     MatInputModule,
     MatDialogModule,
     ReactiveFormsModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    MatToolbarModule,
+    MatSelectModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/restuarant/restuarant.component.ts
+++ src/app/restuarant/restuarant.component.ts
@@ -6,10 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./restuarant.component.css']
 })
 export class RestuarantComponent implements OnInit {
-
+  cards = [{title: 'Number1'}, {title: 'Number2'}, {title: 'Number3'}, {title: 'Number4'}, {title: 'Number5'}, {title: 'Number6'}, {title: 'Number7'}, {title: 'Number8'}, {title: 'Number9'}, {title: 'Number10'}, {title: 'Number11'}, {title: 'Number12'}, {title: 'Number13'}, {title: 'Number14'}, {title: 'Number15'}, {title: 'Number16'}, {title: 'Number17'}, {title: 'Number18'}, {title: 'Number19'}, {title: 'Number20'}, {title: 'Number21'}, {title: 'Number22'}, {title: 'Number23'}, {title: 'Number24'}, {title: 'Number25'}, {title: 'Number26'}, {title: 'Number27'}, {title: 'Number28'}, {title: 'Number29'}, {title: 'Number30'}];
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
